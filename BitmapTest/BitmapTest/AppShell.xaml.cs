f090753c9092007c9787a1ab9eee30f1eea77dd4
Vishal Omprasad <vishal.omprasad@syncfusion.com>
2025-12-19 19:18:54 +0530

    Android Bitmap ByteCount issue sample added

---
+++ BitmapTest/BitmapTest/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BitmapTest
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BitmapTest/BitmapTest/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿#if ANDROID
+using Android.Graphics;
+using Android.Graphics.Drawables;
+using Android.Widget;
+using Javax.Annotation.Meta;
+using System.Reflection;
+#endif
+
+namespace BitmapTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // When loading Embedded image as Stream or Resource, any of the below codes (either FromResource() or FromStream())
+
+            //this.image.Source = ImageSource.FromResource("BitmapTest.Resources.Images.bmpimage.bmp");
+
+            // OR
+
+            //Assembly assembly = Assembly.GetExecutingAssembly();
+            //this.image.Source = ImageSource.FromStream(() => assembly.GetManifestResourceStream("BitmapTest.Resources.Images.bmpimage.bmp"));
+
+        }
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+#if ANDROID
+            Android.Graphics.Bitmap? bitmapImage = GetImageBitmapView(image1);
+#endif
+        }
+
+#if ANDROID
+
+        private Android.Graphics.Bitmap? GetImageBitmapView(Image image)
+        {
+            Android.Graphics.Bitmap? processedBitmap = null;
+            if ((image.Handler.PlatformView is ImageView imageView))
+            {
+                if (imageView.Drawable is BitmapDrawable drawable)
+                {
+                    processedBitmap = drawable.Bitmap?.Copy(drawable.Bitmap.GetConfig(), true);
+                }
+
+                if (processedBitmap == null && Bitmap.Config.Argb8888 != null)
+                {
+                    processedBitmap = Bitmap.CreateBitmap(imageView.Width, imageView.Height, Bitmap.Config.Argb8888);
+                    if (processedBitmap != null)
+                    {
+                        Canvas canvas = new Canvas(processedBitmap);
+                        imageView.Draw(canvas);
+                    }
+                }
+            }
+
+            return processedBitmap;
+        }
+#endif
+
+    }
+}
