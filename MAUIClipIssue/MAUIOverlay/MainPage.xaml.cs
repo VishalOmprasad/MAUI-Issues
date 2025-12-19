fc47f94fc11b0603541f1a582523615d4564d7ae
Vishal Omprasad <vishal.omprasad@syncfusion.com>
2025-12-19 19:35:56 +0530

    Maui Border Background overlay clip issue sample added

---
+++ MAUIClipIssue/MAUIOverlay/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Maui.Controls.Shapes;
+
+namespace MAUIOverlay
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class CustomView : StackLayout
+    {
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            this.Clip = new RoundRectangleGeometry { Rect = new Rect(0, 0, width, height)};
+            base.OnSizeAllocated(width, height);
+        }
+    }
+}
